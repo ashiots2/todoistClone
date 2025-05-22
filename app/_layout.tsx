3ff4d055f821a2de3d8bc9e019e8fabab0444e39
Savvas Ashiotis <ashiots2@gmail.com>
2025-05-23 00:07:35 +0300

    checkpoint <ClerkLoaded> sucks balls

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,41 @@
+import { Colors } from "@/constants/Colors";
+import { tokenCache } from "@/utils/cache";
+import { ClerkLoaded, ClerkProvider } from "@clerk/clerk-expo";
 import { Stack } from "expo-router";
+import React from "react";
+
+const CLERK_PUBLISHABLE_KEY = process.env
+  .EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY as string;
+if (!CLERK_PUBLISHABLE_KEY) {
+  throw new Error(
+    "Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env"
+  );
+} else {
+  console.log("CLERK_PUBLISHABLE_KEY", CLERK_PUBLISHABLE_KEY);
+}
+
+const InitialLayout = () => {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        contentStyle: { backgroundColor: Colors.background },
+      }}
+    >
+      <Stack.Screen name="index" />
+    </Stack>
+  );
+};
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ClerkProvider
+      publishableKey={CLERK_PUBLISHABLE_KEY!}
+      tokenCache={tokenCache}
+    >
+      <ClerkLoaded>
+        <InitialLayout />
+      </ClerkLoaded>
+    </ClerkProvider>
+  );
 }
